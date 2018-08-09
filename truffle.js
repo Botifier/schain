a6481a53d5a193327c889c8971bb746beb4cfeeb
Botifier <mouath.sai.ensi@gmail.com>
2018-08-09 18:14:15 +0100

    first commit

---
+++ truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+  networks: {
+     development: {
+       host: "localhost",
+       gasPrice: 2,
+       gas: 4000000,
+       port: 9545,
+       network_id: "*" // Match any network id
+     }
+   }
+
+};
